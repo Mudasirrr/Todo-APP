0f80b7b40545eafe16f842b978bca11de2ecefe1
ansaamjad <ansaamjad555@gmail.com>
2020-06-26 13:24:09 +0500

    Updates

---
+++ precache-manifest.8c04e6b0577cfdc552540a9de23fb1b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70659e4e88e8cfd8333e6321d3b94863",
+    "url": "/Todo-APP/index.html"
+  },
+  {
+    "revision": "0eccc3066fe3b7660dde",
+    "url": "/Todo-APP/static/js/2.25225961.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-APP/static/js/2.25225961.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5351ffffd74b12315ed6",
+    "url": "/Todo-APP/static/js/main.ec7228f6.chunk.js"
+  },
+  {
+    "revision": "4f17c52c518dd317f810",
+    "url": "/Todo-APP/static/js/runtime-main.4732fbc3.js"
+  }
+]);
